1767df70ecd41280cea576bb469101060365d96a
PirateX0 <295528322@qq.com>
2018-09-30 15:55:30 +0800

    Add project files.

---
+++ 代码转换器/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace 代码转换器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(23, 54);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtCode.Size = new System.Drawing.Size(285, 346);
+            this.txtCode.TabIndex = 0;
+            this.txtCode.WordWrap = false;
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(340, 54);
+            this.txtString.Multiline = true;
+            this.txtString.Name = "txtString";
+            this.txtString.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtString.Size = new System.Drawing.Size(285, 346);
+            this.txtString.TabIndex = 2;
+            this.txtString.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "原始代码";
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(107, 12);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(58, 27);
+            this.btnChange.TabIndex = 4;
+            this.btnChange.Text = "转换";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "代码生成器所需要的字符串";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 438);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtString);
+            this.Controls.Add(this.txtCode);
+            this.Name = "Form1";
+            this.Text = "代码转换器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 代码转换器/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 代码转换器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            string code = txtCode.Text;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("@\"");
+            foreach (var charCode in code)
+            {
+                if (charCode == '"')
+                {
+                    sb.Append("{0}");
+                    continue;
+                }
+                if (charCode == '{')
+                {
+                    sb.Append("{{");
+                    continue;
+                }
+                if (charCode == '}')
+                {
+                    sb.Append("}}");
+                    continue;
+                }
+                sb.Append(charCode);
+            }
+            sb.Append("\",\"\\\"\"");
+            txtString.Text = sb.ToString();
+        }
+    }
+}
